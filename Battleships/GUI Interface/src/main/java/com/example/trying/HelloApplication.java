536385d87448be3c9ed2e820ca5acccaf98ae0d1
Yassine <YassineBibi2002@gmail.com>
2022-07-05 15:48:54 +0200

    Pdf File über die Woche

--- Battleships/GUI Interface/src/main/java/com/example/trying/HelloApplication.java
+++ Battleships/GUI Interface/src/main/java/com/example/trying/HelloApplication.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
