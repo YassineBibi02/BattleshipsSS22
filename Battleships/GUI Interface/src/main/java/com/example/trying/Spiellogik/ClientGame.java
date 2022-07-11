66a1ae27d77f89bc1b7a59390bfac1b73c551a85
Yassine <YassineBibi2002@gmail.com>
2022-07-11 17:28:38 +0200

    Bugs

--- Battleships/GUI Interface/src/main/java/com/example/trying/Spiellogik/ClientGame.java
+++ Battleships/GUI Interface/src/main/java/com/example/trying/Spiellogik/ClientGame.java
@@ -127,7 +127,7 @@ public class ClientGame {
                     IpController.playControl.PreviousMessage += "\nYou Won! ( Please leave xD ) ";
                  IpController.playControl.Chat.setText(IpController.playControl.PreviousMessage);
                  IpController.playControl.Chat.setScrollTop(Double.MAX_VALUE);
-                 IpController.playControl.changeScene( "Lost.fxml");
+                 IpController.playControl.changeScene( "YouWin.fxml");
                     Client_Thread.Stop=true;
 
                     break;
